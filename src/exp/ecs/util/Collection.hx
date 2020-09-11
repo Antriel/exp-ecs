2ec6c85e894ec152d599bb4d49d2be777fd37853
Antriel <Antriel@users.noreply.github.com>
2020-09-11 11:09:26 +0200

    don't use tink_state for collection updates
    it's simple enough without using observable, all updates also need to happen immediately (we could bind directly to prevent tink_state from batching)

--- src/exp/ecs/util/Collection.hx
+++ src/exp/ecs/util/Collection.hx
@@ -1,45 +1,42 @@
 package exp.ecs.util;
 
-import tink.state.*;
-
 using tink.CoreApi;
 
 class Collection<Item> {
 	var pending:Array<Pair<Item, Operation>>;
-	var locked:State<Bool>;
-	
+	var locked:Bool;
+
 	public function new() {
 		pending = [];
-		locked = new State(false);
+		locked = false;
 	}
-	
+
 	public inline function lock() {
-		locked.set(true);
+		locked = true;
 	}
-	
+
 	public inline function unlock() {
-		locked.set(false);
+		locked = false;
+		update();
 	}
-	
+
 	public function destroy() {}
-	
+
 	function schedule(item:Item, operation:Operation) {
-		if(locked.value) {
-			if(pending.length == 0)
-				locked.observe().nextTime(function(v) return !v).handle(update);
+		if (locked) {
 			pending.push(new Pair(item, operation));
 		} else {
 			operate(item, operation);
 		}
 	}
-	
-	function update(_) {
-		for(v in pending) operate(v.a, v.b);
+
+	function update() {
+		for (v in pending)
+			operate(v.a, v.b);
 		pending = [];
 	}
-	
+
 	function operate(item:Item, operation:Operation) {}
-	
 }
 
 enum abstract Operation(Int) {
